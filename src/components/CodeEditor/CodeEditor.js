1dd310bf8841e3bc179646b88812f6e226e897cf
sebaughman <sarahbaughman@u.boisestate.edu>
2018-06-08 16:05:10 -0600

    added empty strings to reducer code on load

--- src/components/CodeEditor/CodeEditor.js
+++ src/components/CodeEditor/CodeEditor.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import brace from 'brace';
 import AceEditor from 'react-ace';
 
 import * as Actions from '../../redux/action-creators'
@@ -10,12 +12,17 @@ import 'brace/theme/monokai'
 class CodeEditor extends Component {
   
   onChange = (newValue) => {
-    this.props.enterCode({[this.props.qID]:newValue})
+    
+    const index = Number(this.props.match.params.questionNumber)
+    const key = `Q${index}`
+    this.props.enterCode({[key]:newValue})
+
   }
 
  // gets qID and assessmentID from parent props
 
   render() {
+    console.log(this.props)
     return (
       <div>
         <AceEditor
@@ -28,7 +35,7 @@ class CodeEditor extends Component {
           showPrintMargin={true}
           showGutter={true}
           highlightActiveLine={true}
-          value={this.props.code[this.props.qID]}
+          value={this.props.code}
           focus={true}
           setOptions={{
             enableBasicAutocompletion: false,
