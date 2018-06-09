a61b73454b847bf00f4247b94850b0ae13af8b53
Tanya Bowerbank <tbowerbank@digis.net>
2018-06-09 12:18:50 -0600

    created email componenet and added CSS

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AssessmentList from './views/AssessmentList/AssessmentList';
 import Wizard from './views/Wizard/Wizard';
 import SplashPage from './views/SplashPage/SplashPage';
 import Submitted from './views/Submitted/Submitted';
+import EnterEmail from './components/EnterEmail/EnterEmail';
 
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
       <Router>
         <Switch>
           <Route exact path='/' component={ SplashPage } />
+          <Route path='/email' component ={ EnterEmail } />
           <Route path='/assessments' component={ AssessmentList }/>
           <Route path='/wizard/:email/:assessmentID/:questionNumber' component={ Wizard }/> 
           <Route path='/confirmation' component = { Submitted } />
