4d4111c72bb2997fe71819fcf876665740cf2f25
Ryan Bart <30918018+rybart@users.noreply.github.com>
2018-06-12 12:20:45 -0600

    added CSS to Question Text

--- src/components/QuestionText/QuestionText.js
+++ src/components/QuestionText/QuestionText.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './QuestionText.css';
 
 
 class QuestionText extends Component {
@@ -8,7 +9,9 @@ class QuestionText extends Component {
   render() {
     return (
       <div>
-        QuestionText
+        <h2 className = 'questionNumber'>Question {}</h2>
+        <p className = 'questionText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
       </div>
     );
   }
