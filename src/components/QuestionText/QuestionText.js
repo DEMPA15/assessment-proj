75aa06db0bd41764b8c09e54fe9c7a2d0373249c
sebaughman <sarahbaughman@u.boisestate.edu>
2018-06-12 19:51:25 -0600

    responseive work

--- src/components/QuestionText/QuestionText.js
+++ src/components/QuestionText/QuestionText.js
@@ -7,12 +7,10 @@ import './questionText.css';
   
   render() {
     return (
-      <div>
-         <div> className="questionText-container"
-          <h2>Question {this.props.qID.split('')[1]} </h2> 
+         <div className="questionText-container"> 
+          <div className='section-title'>Question {this.props.qID.split('')[1]} </div> 
           <p> {this.props.questions[this.props.qIndex].qText} </p>
-          </div>
-      </div>    
+        </div>
     );
   }
 }
