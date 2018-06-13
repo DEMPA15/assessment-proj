8c53f435caae3ddcbf6d46e6c67b1277797e54ed
Tanya Bowerbank <tbowerbank@digis.net>
2018-06-12 19:14:04 -0600

    fixed problems

--- src/components/QuestionText/QuestionText.js
+++ src/components/QuestionText/QuestionText.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import './questionText.css';
 
  class QuestionText extends Component {
   
   render() {
     return (
       <div>
-         <div>
+         <div> className="questionText-container"
           <h2>Question {this.props.qID.split('')[1]} </h2> 
           <p> {this.props.questions[this.props.qIndex].qText} </p>
           </div>
