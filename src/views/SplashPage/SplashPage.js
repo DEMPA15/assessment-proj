0c4cd55a795e1aec2aac439ab8663c46ca2773bf
Ryan Butler <b6utle5r@gmail.com>
2018-06-13 18:02:27 -0600

    splashpage update

--- src/views/SplashPage/SplashPage.js
+++ src/views/SplashPage/SplashPage.js
@@ -8,6 +8,8 @@ class SplashPage extends Component {
     this.props.history.push('/email')
   }
 
+//background gray text white rounded corners
+
   render() {
     return (
       <div className="splash-main">
