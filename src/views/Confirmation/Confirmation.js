06350a22fc7594b11c82adef702d0de22cf7b771
Ryan Butler <b6utle5r@gmail.com>
2018-06-14 20:34:55 -0600

    updated css

--- src/views/Confirmation/Confirmation.js
+++ src/views/Confirmation/Confirmation.js
@@ -10,7 +10,7 @@ class Confirmation extends Component {
       <div className="confirmation">
       <img src={img} alt="code bar logo" className="confirmation-img"/>
       <div className="confirmation-text">
-        <h2>Congratulations {this.props.user.name},</h2><p>your assessment results have been successfully submitted.</p>
+        <h2>Congratulations {this.props.user.name},</h2><p>your assessment results have been successfully submitted. You may now closer browser</p>
         </div>
       <img src={passed} alt="code bar logo" className="confirmation-passed-img"/>
       </div>
