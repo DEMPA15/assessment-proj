743ee40c294ba4b3f6bef72b52a3579dff3711d8
winniethejedi <mcraigparker123@gmail.com>
2018-06-28 19:29:48 -0600

    added another question

--- possible-assessments/array-assessment.js
+++ possible-assessments/array-assessment.js
@@ -48,6 +48,53 @@ console.log(returnSortedArray(alphabeticArray));
 
 console.log(returnSortedArray(numericArray));
 
+// 4. Write a function that takes in two arrays: the first array contains the correct answers to an exam, like ["a", "a", "b", "d"], and the second contains a student's answers.
+
+// The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer (empty string).
+
+// If the score is less than 0, return 0.
+
+// For example:
+
+// checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+// checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+// checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+// checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
+
+// https://www.codewars.com/kata/check-the-exam/train/javascript
+
+// Test 1 - Returns a number
+
+// Test 2 - Returns the correct answer
+
+function checkExam(array1, array2) {
+    let score = 0;
+
+    array2.forEach((answer, i) => {
+        if (answer === '') {
+            score += 0;
+        }
+        else if (answer === array1[i]) {
+            score += 4;
+        }
+        else if (answer !== array1[i]) {
+            score -= 1;
+        }
+    })
+
+    if (score < 0) {
+        return 0
+    }
+    else {
+        return score;
+        }
+   };
+
+console.log(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]))
+console.log(checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]))
+console.log(checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]))
+console.log(checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]))
+
 // 5. Write a JavaScript program to remove duplicate items from an input array (ignore case sensitivity).
 
 // https://www.w3resource.com/javascript-exercises/javascript-array-exercise-14.php
@@ -69,9 +116,6 @@ function removeDuplicates(input) {
     let j = 0;
     for (let i = 0; i < len; i++) {
         let item = arr[i];
-        if (typeof item === 'string') {
-            
-        }
         if (seen[item] !== 1) {
             seen[item] = 1;
             out[j++] = item;
