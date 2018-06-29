87bcb7e07bab7b3ff798e9c327dfd346d1df6c63
Tanya Bowerbank <tbowerbank@digis.net>
2018-06-28 18:50:09 -0600

    proxy and autocompletion

--- src/components/CodeEditor/CodeEditor.js
+++ src/components/CodeEditor/CodeEditor.js
@@ -180,8 +180,8 @@ class CodeEditor extends Component {
           value={this.state[this.props.qID]}
           focus={true}
           setOptions={{
-            enableBasicAutocompletion: false,
-            enableLiveAutocompletion: false,
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true,
             enableSnippets: true,
             showLineNumbers: true,
             tabSize: 2,
